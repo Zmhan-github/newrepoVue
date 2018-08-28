7f8babf7f46948f7494cd37e5e5fc7cc66cb5647
Marat <marat@deex.exchange>
2018-08-28 10:55:52 +0500

    steps-include

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VModal from 'vue-js-modal';
 
 import App from './App.vue';
 import router from './router';
 
 
-// Vue.config.productionTip = false;
+Vue.config.productionTip = false;
 
-// https://api.account.otau.org
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://192.168.100.199/v2',
+});
+
+Vue.use(VModal, { dialog: true });
+
+// https://api.account.otau.org - Server otau
+
+
 new Vue({
   render: h => h(App),
   router,
+  axios,
 }).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,65 +1,68 @@
 <template>
   <div class="home">
-        <form v-if="isLogin === 'regStep1'" @submit.prevent="registerStep1" >
-          <phone-input :phone="phone"/>
-          <button>Далее</button><br />
-          <li><a @click="toLogin">У меня есть аккаунт</a></li>
-        </form>
-        <form v-else-if="isLogin === 'regStep2'" @submit.prevent="registerStep2" >
-          <input id="code" type="text" v-model="register.codeSMS" value="register.codeSMS" placeholder="Код подтверждения">
-          <button>Далее</button> <br />
-          <li><a @click="toLogin">У меня есть аккаунт</a></li>
-        </form>
-        <!--
-        <form v-else-if="isLogin === 'regStep3'" @submit.prevent="registerStep3" >
-          <ul>
-          <li>
-            <img src="../assets/name.png">
-            <input type="text" v-model.trim="register.name" placeholder="Введите имя" @input="changeName">
-            <span class="wr" v-if="msgValid.name">Заполните это поле.</span>
-          </li>
-          <li>
-            <img src="../assets/sname.png">
-            <input type="text" v-model.trim="register.lastname" placeholder="Введите фамилию" @input="changeLastName">
-            <span class="wr" v-if="msgValid.lastname">Заполните это поле.</span>
-          </li>
-          <li>
-            <div class="boxB">
-              <img src="../assets/calendar.png">
-            </div>
-            <div class="dateB">
-              <datepicker v-model="date"></datepicker>
-            </div>
-          </li>
-          <li class="mg-5">
-            <img src="../assets/gender.png">
-          <select v-model="selected">
-            <option disabled value="">Выберите пол</option>
-            <option>Мужской</option>
-            <option>Женский</option>
-          </select>
-          </li>
-          <li>
-            <img src="../assets/password.png">
-            <input id="name" type="password" v-model="register.password" @input="changePass" placeholder="Введите пароль"> <br />
-            <span class="wr" v-if="msgValid.password">Введите пароль</span>
-            <span class="wr" v-if="msgValid.minSimbol">Минимум 8-символов.</span>
-          </li>
-          <li>
-            <img src="../assets/password.png">
-            <input id="name" type="password" v-model="register.confirmPassword" @input="changePass2" placeholder="Повторите пароль"> <br />
-            <span class="wr" v-if="msgValid.confirmPassword">Повторите пароль.</span>
-            <span class="wr" v-if="msgValid.passAndpassConf">Пароли не совподают</span>
-          </li>
-          <li><button type="submit">Регистрация</button></li>
-          <li><a @click="toLogin">У меня есть аккаунт</a></li>
-          </ul>
-        </form> -->
+    <v-dialog name="login" :width="250" :height="200" />
+    <form v-if="isLogin === 'regStep1'" @submit.prevent="registerStep1">
+      <phone-input :phone="phone"/>
+      <button>Далее</button><br />
+      <li><a @click="ChangePassword">У меня есть аккаунт</a></li>
+    </form>
+
+     <form v-else-if="isLogin === 'regStep2'" @submit.prevent="registerStep2" >
+        <input id="code" type="text" v-model="register.codeSMS" value="register.codeSMS" placeholder="Код подтверждения">
+        <button>Далее</button> <br />
+        <li><a @click="ChangePassword">У меня есть аккаунт</a></li>
+      </form>
+
+    <form v-else-if="isLogin === 'regStep3'" @submit.prevent="registerStep3" >
+      <ul>
+      <li>
+        <img src="../assets/name.png">
+        <input type="text" v-model.trim="register.name" placeholder="Введите имя" @input="changeName">
+        <span class="wr" v-if="msgValid.name">Заполните это поле.</span>
+      </li>
+      <li>
+        <img src="../assets/sname.png">
+        <input type="text" v-model.trim="register.lastname" placeholder="Введите фамилию" @input="changeLastName">
+        <span class="wr" v-if="msgValid.lastname">Заполните это поле.</span>
+      </li>
+      <li>
+        <div class="boxB">
+          <img src="../assets/calendar.png">
+        </div>
+        <div class="dateB">
+          <datepicker v-model="date"></datepicker>
+        </div>
+      </li>
+      <li class="mg-5">
+        <img src="../assets/gender.png">
+      <select v-model="selected">
+        <option disabled value="">Выберите пол</option>
+        <option>Мужской</option>
+        <option>Женский</option>
+      </select>
+      </li>
+      <li>
+        <img src="../assets/password.png">
+        <input id="name" type="password" v-model="register.password" @input="changePass" placeholder="Введите пароль"> <br />
+        <span class="wr" v-if="msgValid.password">Введите пароль</span>
+        <span class="wr" v-if="msgValid.minSimbol">Минимум 8-символов.</span>
+      </li>
+      <li>
+        <img src="../assets/password.png">
+        <input id="name" type="password" v-model="register.confirmPassword" @input="changePass2" placeholder="Повторите пароль"> <br />
+        <span class="wr" v-if="msgValid.confirmPassword">Повторите пароль.</span>
+        <span class="wr" v-if="msgValid.passAndpassConf">Пароли не совподают</span>
+      </li>
+      <li><button type="submit">Регистрация</button></li>
+      <li><a @click="ChangePassword">У меня есть аккаунт</a></li>
+      </ul>
+    </form>
   </div>
 </template>
 
 <script>
 import PhoneInput from 'vue2-phone-input';
+
 
 export default {
   name: 'home',
@@ -98,8 +101,74 @@ export default {
       loginValidate: {
         login: false,
       },
-    }
+    };
   },
+
+  //================== Methods ==========================/
+  methods: {
+
+    // ================ REGister Step - 1 ===============//
+    registerStep1() {
+ 
+      this.$http.post('/signup', { phone: this.phone.number })
+      .then(res => {
+        this.isLogin = 'regStep2';
+      })
+      .catch(err => {
+        this.$modal.show('dialog', {
+        text: 'Введите валидный номер телефона!',
+        buttons: [
+          {
+            title: 'Закрыть'
+          }
+        ]});
+      });
+
+
+    },
+
+    // ================= Register Step - 2 ===============//
+    registerStep2() {
+      let codeSMS = this.register.codeSMS.trim();
+      if (codeSMS.length > 5 || codeSMS.length < 5) {
+         this.$modal.show('dialog', {
+            text: 'Введите 5 - символов!',
+            buttons: [
+              {
+                title: 'Закрыть'
+              }
+          ]});
+      } else {
+          this.$http.post('/signup/verify-phone', {
+            phone: this.phone.number,
+            code: this.register.codeSMS,
+          })
+          .then(res => {
+            this.register.token = res.data.access_token;
+            this.isLogin = 'regStep3';
+          })
+          .catch(err => {
+            this.$modal.show('dialog', {
+            text: 'Ошибка!',
+            buttons: [
+              {
+                title: 'Закрыть'
+              }
+          ]});
+          });
+      }
+    },
+   
+
+    // =========== Change Password ============ //
+    ChangePassword() {
+      console.log('You clicked Button!');
+    },
+
+
+
+  },
+};
   //================== Login Validate ===================/
   // computed: {
   //   loginValidateChange() {
@@ -336,19 +405,13 @@ export default {
     //     });
     // },
   // },
-};
+
 </script>
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* .v--modal-overlay {
-  position: fixed;
-  box-sizing: border-box;
-  left: 0;
-  top: 0;
-  background: none;
-} */
+
 .mg-5 {
   margin: 10px 0;
 }
